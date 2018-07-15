b2cc4863819b82c49862fb1e5841dfa64c4a858c
u3shit <u3shit@gmail.com>
2018-07-15 17:47:10 +0200

    improve iwyu script

--- iwyu.sh
+++ iwyu.sh
@@ -1,23 +1,35 @@
 #! /bin/bash
 
 if [[ $# < 1 ]]; then
-    echo "Usage: $0 [--accept] file" >&2
+    echo "Usage: $0 [--accept] file [extra clang args...]" >&2
     echo "Check all files: find src test -name '*.[ch]pp' -print0 | xargs -0P0 -n1 $0" >&2
     exit 255
 fi
 
-FLAGS=(
+dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+base_dir="${base_dir-$dir}"
+base_dir_escaped="$(sed 's/[[*?\]/\\\0/g' <<<"$base_dir")"
+
+flags+=(
     -std=c++17 -stdlib=libc++
-    -Xiwyu --mapping_file=iwyu.imp
-    -Isrc -isystem ext/brigand/include -isystem ext/lua-5.3.4/src
-    -isystem ext/catch/include
-    -I../build/clang-debug/libshit/src -I../build/libshit/src
+    -Xiwyu --mapping_file="$dir/iwyu.imp"
+    -I"$dir/src"
+    -I"$dir/../build/clang-debug/libshit/src"
+    -I"$dir/../build/libshit/src"
+    -isystem "$dir/ext/brigand/include"
+    -isystem "$dir/ext/lua-5.3.4/src"
+    -isystem "$dir/ext/catch/include"
     -Wno-parentheses -Wno-dangling-else
 )
 
 accept=false
+verbose=false
 if [[ $1 == --accept ]]; then
     accept=true
+    shift
+fi
+if [[ $1 == -v || $1 == --verbose ]]; then
+    verbose=true
     shift
 fi
 
@@ -26,24 +38,28 @@ file="$1"
 [[ $file == *.binding.hpp ]] && exit 0
 
 succ=false
-out="$(include-what-you-use "${FLAGS[@]}" "$file" 2>&1)"
-[[ $? -eq 2 ]] && succ=true
+out="$($verbose && set -x; include-what-you-use "${flags[@]}" "$@" 2>&1)"
+$verbose && cat <<<"$out"
 
+[[ $? -eq 2 ]] && succ=true
+tail -n1 <<<"$out" | grep -q 'has correct #includes/fwd-decls)' && succ=true
+
+out="${out//$base_dir_escaped\/}"
 if $accept; then
     if $succ; then
         rm -f "$file".iwyu_out
     else
-        echo "$out" > "$file".iwyu_out
+        cat <<<"$out" > "$file".iwyu_out
     fi
     exit 0
 elif [[ -f "$file".iwyu_out ]]; then
-    diff -u "$file".iwyu_out <(echo "$out")
+    diff -u "$file".iwyu_out <(cat <<<"$out")
     [[ $? -eq 0 ]] && exit 0
 elif $succ; then
     exit 0
 fi
 
 echo -e '\033[33m'"$1"' output\033[0m'
-echo "$out"
+cat <<<"$out"
 echo -e '\033[31;1m'"$1"' failed\033[0m'
 exit 1
