5e6cc1c3be73bd82dc6a5fddb77b1306a85258e5
u3shit <u3shit@gmail.com>
2020-07-20 22:07:56 +0200

    tracy: fix gcc warnings due to empty statements in no-tracy mode

--- ext/no-tracy/Tracy.hpp
+++ ext/no-tracy/Tracy.hpp
@@ -3,66 +3,66 @@
 #pragma once
 
 // frames
-#define FrameMark
-#define FrameMarkNamed(name)
-#define FrameMarkStart(name)
-#define FrameMarkEnd(name)
-#define FrameImage(image, width, height, offset, flip)
+#define FrameMark (void) 0
+#define FrameMarkNamed(name) (void) 0
+#define FrameMarkStart(name) (void) 0
+#define FrameMarkEnd(name) (void) 0
+#define FrameImage(image, width, height, offset, flip) (void) 0
 
 // zones
-#define ZoneScoped
-#define ZoneScopedC(color)
-#define ZoneScopedN(name)
-#define ZoneScopedNC(name, color)
-#define ZoneScopedS(depth)
-#define ZoneScopedCS(color, depth)
-#define ZoneScopedNS(name, depth)
-#define ZoneScopedNCS(name, color, depth)
-#define ZoneText(text, size)
-#define ZoneValue(val)
-#define ZoneName(text, size)
+#define ZoneScoped (void) 0
+#define ZoneScopedC(color) (void) 0
+#define ZoneScopedN(name) (void) 0
+#define ZoneScopedNC(name, color) (void) 0
+#define ZoneScopedS(depth) (void) 0
+#define ZoneScopedCS(color, depth) (void) 0
+#define ZoneScopedNS(name, depth) (void) 0
+#define ZoneScopedNCS(name, color, depth) (void) 0
+#define ZoneText(text, size) (void) 0
+#define ZoneValue(val) (void) 0
+#define ZoneName(text, size) (void) 0
 
-#define ZoneNamed(var, active)
-#define ZoneNamedC(var, color, active)
-#define ZoneNamedN(var, name, active)
-#define ZoneNamedNC(var, name, color, active)
-#define ZoneTextV(var, text, size)
-#define ZoneValueV(var, val)
-#define ZoneNameV(var, text, size)
+#define ZoneNamed(var, active) (void) 0
+#define ZoneNamedC(var, color, active) (void) 0
+#define ZoneNamedN(var, name, active) (void) 0
+#define ZoneNamedNC(var, name, color, active) (void) 0
+#define ZoneTextV(var, text, size) (void) 0
+#define ZoneValueV(var, val) (void) 0
+#define ZoneNameV(var, text, size) (void) 0
 
 // locks
 #define TracyLockable(type, var) type var
 #define TracyLockableN(type, var, desc) type var
-#define LockableName(var, name, size)
+#define LockableName(var, name, size) (void) 0
 #define LockableBase(type) type
-#define LockMark(var)
+#define LockMark(var) (void) 0
 
 #define TracySharedLockable(type, var) type var
 #define TracySharedLockableN(type, var, desc) type var
 #define SharedLockableBase(type) type
 
 // plot
-#define TracyPlot(name, value)
-#define TracyPlotConfig(name, format)
+#define TracyPlot(name, value) (void) 0
+#define TracyPlotConfig(name, format) (void) 0
 
 // msg log
-#define TracyMessage(text, size)
-#define TracyMessageL(text)
-#define TracyMessageC(text, size, color)
-#define TracyMessageLC(text, color)
-#define TracyMessageS(text, size, depth)
-#define TracyMessageLS(text, depth)
-#define TracyMessageCS(text, size, color, depth)
-#define TracyMessageLCS(text, color, depth)
+#define TracyMessage(text, size) (void) 0
+#define TracyMessageL(text) (void) 0
+#define TracyMessageC(text, size, color) (void) 0
+#define TracyMessageLC(text, color) (void) 0
+#define TracyMessageS(text, size, depth) (void) 0
+#define TracyMessageLS(text, depth) (void) 0
+#define TracyMessageCS(text, size, color, depth) (void) 0
+#define TracyMessageLCS(text, color, depth) (void) 0
 
 // app info
-#define TracyAppInfo(text, size)
+#define TracyAppInfo(text, size) (void) 0
 
 // memory
-#define TracyAlloc(ptr, size)
-#define TracyFree(ptr)
-#define TracyAllocS(ptr, size, depth)
-#define TracyFreeS(ptr, depth)
+#define TracyAlloc(ptr, size) (void) 0
+#define TracyFree(ptr) (void) 0
+#define TracyAllocS(ptr, size, depth) (void) 0
+#define TracyFreeS(ptr, depth) (void) 0
 
 // thread
 namespace tracy
