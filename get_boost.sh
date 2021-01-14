c06c99debebd7c9ad520069b1904d693f57e099c
u3shit <u3shit@gmail.com>
2021-01-14 20:56:22 +0100

    update boost to 1.75

--- get_boost.sh
+++ get_boost.sh
@@ -3,12 +3,12 @@
 set -ex
 cd "$(dirname "$0")"
 
-VER=1.73.0
+VER=1.75.0
 _VER=$(echo $VER | sed 's/\./_/g')
 TGT=ext/boost
 DL=https://dl.bintray.com/boostorg/release/$VER/source/boost_$_VER.tar.bz2
 BZ2_FILE=ext/boost_$_VER.tar.bz2
-SHA256=4eb3b8d442b426dc35346235c8733b5ae35ba431690e38c6a8263dce9fcbb402
+SHA256=953db31e016db7bb207f11432bef7df100516eeb746843fa0486a222e3fd49cb
 
 if [ -e "$TGT" ]; then
     echo "$(pwd)/$TGT exists, refusing to continue" >&2
