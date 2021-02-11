f538886ac56d712b6e6ef0e0f41a9650fb8cc8ad
u3shit <u3shit@gmail.com>
2021-02-12 00:51:56 +0100

    platform: extra helper macros for oneline only on this platform/not on this platform codes
    e.g. instead of
    
        #if LIBSHIT_OS_IS_LINUX
        foobar
        #endif
    
    you can write
    
        LIBSHIT_OS_LINUX(foobar);
    
    Can be useful inside macros too, where you can't easily put #if/#endif.
    For more complex conditions, you either have to use #if/#elif/#else/#endif, or
    experiment with Boost PP (BOOST_PP_IIF, BOOST_PP_BITOR, BOOST_PP_COMPL, ...).

--- src/libshit/platform.hpp
+++ src/libshit/platform.hpp
@@ -4,20 +4,32 @@
 
 #ifdef __linux__
 #define LIBSHIT_OS_IS_LINUX 1
+#define LIBSHIT_OS_LINUX(...) __VA_ARGS__
+#define LIBSHIT_OS_NOT_LINUX(...)
 #else
 #define LIBSHIT_OS_IS_LINUX 0
+#define LIBSHIT_OS_LINUX(...)
+#define LIBSHIT_OS_NOT_LINUX(...) __VA_ARGS__
 #endif
 
 #ifdef __vita__
 #define LIBSHIT_OS_IS_VITA 1
+#define LIBSHIT_OS_VITA(...) __VA_ARGS__
+#define LIBSHIT_OS_NOT_VITA(...)
 #else
 #define LIBSHIT_OS_IS_VITA 0
+#define LIBSHIT_OS_VITA(...)
+#define LIBSHIT_OS_NOT_VITA(...) __VA_ARGS__
 #endif
 
 #ifdef _WIN32
 #define LIBSHIT_OS_IS_WINDOWS 1
+#define LIBSHIT_OS_WINDOWS(...) __VA_ARGS__
+#define LIBSHIT_OS_NOT_WINDOWS(...)
 #else
 #define LIBSHIT_OS_IS_WINDOWS 0
+#define LIBSHIT_OS_WINDOWS(...)
+#define LIBSHIT_OS_NOT_WINDOWS(...) __VA_ARGS__
 #endif
 
 #if __has_include(<yvals.h>)
@@ -25,14 +37,22 @@
 #endif
 #ifdef _CPPLIB_VER
 #define LIBSHIT_STDLIB_IS_MSVC 1
+#define LIBSHIT_STDLIB_MSVC(...) __VA_ARGS__
+#define LIBSHIT_STDLIB_NOT_MSVC(...)
 #else
 #define LIBSHIT_STDLIB_IS_MSVC 0
+#define LIBSHIT_STDLIB_MSVC(...)
+#define LIBSHIT_STDLIB_NOT_MSVC(...) __VA_ARGS__
 #endif
 
 #ifndef NDEBUG
 #define LIBSHIT_IS_DEBUG 1
+#define LIBSHIT_DEBUG(...) __VA_ARGS__
+#define LIBSHIT_NOT_DEBUG(...)
 #else
 #define LIBSHIT_IS_DEBUG 0
+#define LIBSHIT_DEBUG(...)
+#define LIBSHIT_NOT_DEBUG(...) __VA_ARGS__
 #endif
 
 #if defined(__SANITIZE_ADDRESS__)
