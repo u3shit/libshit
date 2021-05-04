u3shit/libshit

commit fd87b739add8268d14f3808f5b3e13941e78a2bb

experimental: support const T in SimpleVector

src/libshit/container/simple_vector.hpp
@@ -29,7 +29,8 @@ namespace Libshit
    * stl implementations, this doesn't choke if size_type/difference_type is
    * only explicit constructible from ints, so it's usable with StrongAllocator.
    */
-  template <typename T, typename Allocator = std::allocator<T>>
+  template <
+    typename T, typename Allocator = std::allocator<std::remove_const_t<T>>>
   class SimpleVector : private Allocator
   {
   private:
@@ -39,6 +40,8 @@ namespace Libshit
     constexpr static inline auto ALIGN =
       std::max(std::size_t(1), (2*sizeof(void*)) / sizeof(T));
 
+    using MutT = std::remove_const_t<T>;
+    using MutPtr = MutT*;
   public:
     using value_type = T;
     using allocator_type = Allocator;
@@ -46,7 +49,9 @@ namespace Libshit
     using difference_type = typename AllocTraits::difference_type;
     using reference = T&;
     using const_reference = const T&;
-    using pointer = typename AllocTraits::pointer;
+    using pointer = std::conditional_t<
+      std::is_const_v<T>, typename AllocTraits::const_pointer,
+      typename AllocTraits::pointer>;
     using const_pointer = typename AllocTraits::const_pointer;
 
     using iterator = pointer;
@@ -292,7 +297,7 @@ namespace Libshit
     // todo insert range
     iterator insert(const_iterator cit, const T& val)
     { return emplace(cit, val); }
-    iterator insert(const_iterator cit, T&& val)
+    iterator insert(const_iterator cit, MutT&& val)
     { return emplace(cit, Move(val)); }
 
     template <typename... Args>
@@ -303,11 +308,11 @@ namespace Libshit
         return std::addressof(emplace_back(std::forward<Args>(args)...));
       LIBSHIT_ASSERT(!empty());
 
-      auto it = const_cast<iterator>(cit);
+      auto it = const_cast<MutT*>(cit);
       if (end_ptr == capacity_ptr)
       {
         iterator res;
-        resize_capacity(get_grow_size(), it, [&](pointer& q)
+        resize_capacity(get_grow_size(), it, [&](MutPtr& q)
         {
           res = q;
           AllocTraits::construct(
@@ -319,7 +324,7 @@ namespace Libshit
       }
       else
       {
-        emplace_back(Libshit::Move(back()));
+        emplace_back(Libshit::Move(end_ptr[-1]));
         std::move_backward(it, end_ptr-2, end_ptr-1);
         *it = T(std::forward<Args>(args)...);
         return it;
@@ -331,8 +336,8 @@ namespace Libshit
     iterator erase(const_iterator cit)
     {
       LIBSHIT_ASSERT(cit >= begin_ptr && cit < end_ptr);
-      auto it = const_cast<iterator>(cit);
-      std::move(it+1, end(), it);
+      auto it = const_cast<MutT*>(cit);
+      std::move(it+1, end_ptr, it);
       pop_back();
       return it;
     }
@@ -340,8 +345,8 @@ namespace Libshit
     iterator erase(const_iterator cfirst, const_iterator clast)
     {
       LIBSHIT_ASSERT(begin_ptr <= cfirst && cfirst <= clast && clast <= end_ptr);
-      auto first = const_cast<iterator>(cfirst);
-      auto last = const_cast<iterator>(clast);
+      auto first = const_cast<MutT*>(cfirst);
+      auto last = const_cast<MutT*>(clast);
       std::move(last, end_ptr, first);
 
       auto new_end = end_ptr - (last-first);
@@ -358,14 +363,14 @@ namespace Libshit
       noexcept(std::is_nothrow_move_assignable_v<T>)
     {
       LIBSHIT_ASSERT(cit >= begin_ptr && cit < end_ptr);
-      auto it = const_cast<iterator>(cit);
-      if (it != &back()) *it = Move(back());
+      auto it = const_cast<MutT*>(cit);
+      if (it != end_ptr-1) *it = Move(end_ptr[-1]);
       pop_back();
       return it;
     }
 
     reference push_back(const T& t) { return emplace_back(t); }
-    reference push_back(T&& t) { return emplace_back(Move(t)); }
+    reference push_back(MutT&& t) { return emplace_back(Move(t)); }
     template <typename... Args>
     reference emplace_back(Args&&... args)
     {
@@ -423,7 +428,7 @@ namespace Libshit
     bool operator==(const SimpleVector& o) const { return !(*this != o); }
 
   private:
-    pointer begin_ptr = nullptr, end_ptr = nullptr, capacity_ptr = nullptr;
+    MutPtr begin_ptr = nullptr, end_ptr = nullptr, capacity_ptr = nullptr;
 
     void asan_annotate(
       const pointer& old_end, int old_offs, const pointer& new_end, int new_offs)
@@ -532,7 +537,7 @@ namespace Libshit
       end_ptr = new_end;
     }
 
-    void clear_to_end(pointer new_end)
+    void clear_to_end(MutPtr new_end)
     {
       for (auto p = new_end; p != end_ptr; ++p)
         AllocTraits::destroy(static_cast<Allocator&>(*this), std::addressof(*p));

test/container/simple_vector.cpp
@@ -50,8 +50,11 @@ namespace Libshit::Test
     { return os << "X{" << x.i << '}'; }
   }
 
-  TEST_CASE_TEMPLATE("basic test", T, int, X)
+  TEST_CASE_TEMPLATE("basic test", T, int, X, const int)
   {
+    constexpr const bool is_const = std::is_const_v<T>;
+    using NoConst = std::remove_const_t<T>;
+
     Libshit::SimpleVector<T> v;
     CHECK(v.empty());
     CHECK(v.size() == 0);
@@ -105,7 +108,7 @@ namespace Libshit::Test
     {
       SUBCASE("empty") {}
       SUBCASE("not empty") { v.push_back(1); }
-      std::vector<T> v2{4,5,6};
+      std::vector<NoConst> v2{4,5,6};
       v.assign(v2.begin(), v2.end());
       CHECK(!v.empty());
       CHECK(v.size() == 3);
@@ -128,11 +131,15 @@ namespace Libshit::Test
       SUBCASE("simple accessors")
       {
         CHECK(v.at(1) == 1);
-        v.at(1) = 9;
+        // that const cast would work even in non-const case, but test that
+        // assignment works normally when members are not const
+        if constexpr (is_const) const_cast<NoConst&>(v.at(1)) = 9;
+        else v.at(1) = 9;
         CHECK(v.at(1) == 9);
         CHECK(std::as_const(v).at(2) == 2);
         CHECK(v[2] == 2);
-        v[2] = 8;
+        if constexpr (is_const) const_cast<NoConst&>(v[2]) = 8;
+        else v[2] = 8;
         CHECK(v[2] == 8);
         CHECK(std::as_const(v)[2] == 8);
         CHECK(v == Libshit::SimpleVector<T>{0, 9, 8, 3});
@@ -142,10 +149,12 @@ namespace Libshit::Test
 
         CHECK(v.front() == 0);
         CHECK(std::as_const(v).front() == 0);
-        v.front() = 5;
+        if constexpr (is_const) const_cast<NoConst&>(v.front()) = 5;
+        else v.front() = 5;
         CHECK(v.back() == 3);
         CHECK(std::as_const(v).back() == 3);
-        v.back() = 6;
+        if constexpr (is_const) const_cast<NoConst&>(v.back()) = 6;
+        else v.back() = 6;
         CHECK(v == Libshit::SimpleVector<T>{5, 9, 8, 6});
       }
 
@@ -316,7 +325,7 @@ namespace Libshit::Test
         CHECK(v == v2);
         CHECK(!(v != v2));
 
-        v2[2] = 7;
+        const_cast<std::remove_const_t<T>&>(v2[2]) = 7;
         CHECK(v != v2);
         CHECK(!(v == v2));
 

commit 08102385000a8b56a6e06322e81c05e0e828fbc4

SimpleVector: not using pointer from allocator traits
regression since fd87b739add8268d14f3808f5b3e13941e78a2bb

src/libshit/container/simple_vector.hpp
@@ -41,7 +41,7 @@ namespace Libshit
       std::max(std::size_t(1), (2*sizeof(void*)) / sizeof(T));
 
     using MutT = std::remove_const_t<T>;
-    using MutPtr = MutT*;
+    using MutPtr = typename AllocTraits::pointer;
   public:
     using value_type = T;
     using allocator_type = Allocator;